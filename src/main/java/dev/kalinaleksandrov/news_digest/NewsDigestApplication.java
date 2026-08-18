5fe75e55919cb9240a91736f36bc11f1e970d071
Kalin <kalin.aleksandrov00@gmail.com>
2026-08-18 21:11:58 +0300

    Initial commit

---
+++ src/main/java/dev/kalinaleksandrov/news_digest/NewsDigestApplication.java
@@ -0,0 +1,13 @@
+package dev.kalinaleksandrov.news_digest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsDigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsDigestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kalinaleksandrov/news_digest/NewsDigestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kalinaleksandrov.news_digest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsDigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
